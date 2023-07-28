70ef309f53e9d07fbe63ec4423eece31a4c1c4c7
Joe Nicke <jnicke@corenttech.com>
2023-07-28 19:14:22 +0000

    Revert "Update to a Working Version"
    This reverts commit b6d61dfa65829845686460510f1f61624d455a68.

--- include/saasops.php
+++ include/saasops.php
@@ -2,21 +2,21 @@
 # ----------------------------------------------------
 # Get DBName / DBHost / do not echo any varibles here |
 # ----------------------------------------------------
-// Program to display URL of current page. 
-if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') 
-	$link = "https"; 
+// Program to display URL of current page.
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+    $link = "https";
 else
-	$link = "http"; 
-// Here append the common URL characters. 
-$link .= "://"; 
-// Append the host(domain name, ip) to the URL. 
-$link = $_SERVER['HTTP_HOST']; 
-// Append the requested resource location to the URL 
-//$link .= $_SERVER['REQUEST_URI']; 
-	
+    $link = "http";
+// Here append the common URL characters.
+$link .= "://";
+// Append the host(domain name, ip) to the URL.
+$link = $_SERVER['HTTP_HOST'];
+// Append the requested resource location to the URL
+//$link .= $_SERVER['REQUEST_URI'];
+
 // Set Domain and Tenant
 $dom1 = trim(strstr($link,"."),".");
-$p1 = strpos($link,"."); 
+$p1 = strpos($link,".");
 $tenant = substr($link,0,$p1);
 $tenantdb = 'aut_' . $tenant;
 define('DBNAME',$tenantdb);
